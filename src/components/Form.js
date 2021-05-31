d797c234c55dccd223c891b133cfeaa0817a8423
Mahima Garg <mahimagarg832@gmail.com>
2021-05-31 19:55:09 +0530

    fgfg

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,32 +8,42 @@ function Form(){
             <div className="contact-details">
             <h1> Contact Us </h1>
             <h3>our mailing address is:</h3>
-              <p>  H.no.-44,abc road,<br />
-                Madhya Pradesh,India<br />
-                Phone- 0987456321</p>
+              <p>  H. No.-44,abc road,<br />
+                Madhya Pradesh, India<br />
+                Phone- 0987456321<br />
+                Email-abs@gmail.com</p>
 </div>
 <div className="details">
             <div className="inputBox">
-            <label> Username </label><br />
+            <label> Username: </label><br />
                     <input type="text" name="username" autocomplete="off" required />
                   
                 </div><br />
 
                 <div className="inputBox">
-                <label> Email </label><br />
-                    <input type="text" name="username" autocomplete="off" required />
+                <label> Email: </label><br />
+                    <input type="email" name="email" autocomplete="off"  required />
               
                 </div><br />
 
-                <div className="inputBox1">
-                <label> Message </label><br/>
+                <div className="inputBox">
+                <label> Phone: </label><br />
+                    <input type="number" name="number" autocomplete="off" required />
+             </div><br />
+
+
+                <div className="inputBox">
+                <label> Message: </label><br/>
                     <textarea type="textbox" name="type here" autocomplete="off" required />
                  
                 </div><br />
-                </div>
-               <a href="">Submit</a> 
- 
-            </div>
+                
+                
+                
+                <button href="/submit" className="button0">Send Message</button>
+               </div>
+          </div>
+          
         
     );
 }
